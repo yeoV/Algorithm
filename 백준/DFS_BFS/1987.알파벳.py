7553e746d6223def7e2d1148cd0dccf05c14ac19
LSY <ys_lee1017@naver.com>
2021-05-26 23:34:20 +0900

    bfs

--- 백준/DFS_BFS/1987.알파벳.py
+++ 백준/DFS_BFS/1987.알파벳.py
@@ -3,11 +3,22 @@ read = sys.stdin.readline
 R, C = map(int, read().split())
 graph = [list(read().rstrip()) for _ in range(R)]
 move = ((0, 1), (0, -1), (1, 0), (-1, 0))
+ans = 1
 
 
-def dfs(depth, now, next):
+def bfs(start_x, start_y, alphabet):
+    global ans
+    q = set([(start_x, start_y, alphabet)])
+    while q:
+        x, y, alpha = q.pop()
+        ans = max(ans, len(alpha))
+        for dxy in move:
+            dx, dy = x + dxy[0], y + dxy[1]
 
-    x, y = next
-    for dxy in move:
-        dx, dy = x+dxy[0], y+dxy[1]
-        # 범위 검사 밑 같지 않은 알파벳일 경우
+            if 0 <= dx < R and 0 <= dy < C and graph[dx][dy] not in alpha:
+                q.add((dx, dy, alpha + graph[dx][dy]))
+            else:
+                continue
+
+bfs(0, 0, graph[0][0])
+print(ans)
