260ed47dfe9fe8a2892337d9c08a9937ea0df89e
LSY <ys_lee1017@naver.com>
2021-07-30 21:57:23 +0900

    스택

--- 백준/자료구조/stack/6198.옥상정원꾸미기.py
+++ 백준/자료구조/stack/6198.옥상정원꾸미기.py
@@ -6,7 +6,9 @@ building = [int(read().rstrip()) for _ in range(n)]
 
 stack = []
 ans = 0
+print(building)
 for idx, val in enumerate(building):
+    print(stack)
     while stack and stack[-1] <= val:
         stack.pop()
     stack.append(val)
