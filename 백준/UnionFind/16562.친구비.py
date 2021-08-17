419d2059789f50b064004fbe12349d61bd122095
LSY <ys_lee1017@naver.com>
2021-08-17 14:34:03 +0900

    Union Find

--- 백준/UnionFind/16562.친구비.py
+++ 백준/UnionFind/16562.친구비.py
@@ -2,25 +2,23 @@
 import sys
 read = sys.stdin.readline
 n, m, k = map(int, read().split())
-friends = list(map(int, read().split()))
-parent = [[i, friends[i-1]] for i in range(n+1)]
+cost = [0] + list(map(int, read().split()))
+nodes = [i for i in range(n+1)]
 # 친구비는 랭크 개념을 도입하면 어떨련지
 def find(x):
-    if parent[x][0] == x:
+    if nodes[x] == x:
         return x
-    parent[x][0] = find(parent[x][0])
-    return parent[x][0]
-
+    nodes[x] = find(nodes[x])
+    return nodes[x]
 def union(x, y):
     x = find(x)
     y = find(y)
-    # 친구비가 더 작은 아이를 부모로 연결
-    if parent[x][1] > parent[y][1]:
-        parent[x][0] = parent[y][0]
-    elif parent[x][1] < parent[y][1]:
-        parent[y][0] = parent[x][0]
+    if cost[x] > cost[y]:
+        nodes[x] = y
+    elif cost[y] > cost[x]:
+        nodes[y] = x
     else:
-        parent[y][0] = parent[x][0]
+        nodes[x] = y
 
 for _ in range(m):
     a, b = map(int, read().split())
@@ -28,8 +26,9 @@ for _ in range(m):
     b = find(b)
     if a != b:
         union(a, b)
-for idx in range(1, n):
-    find(idx)
-parent_set = set(map(lambda x: x[0], parent[1:]))
-total_fee = sum(map(lambda x : friends[x-1], parent_set))
-print(total_fee) if not total_fee > k else print('Oh no')
+
+fee = 0
+for idx in range(1,n+1):
+    if idx == nodes[idx]:
+        fee += cost[idx]
+print(fee if fee <= k else 'Oh no')
