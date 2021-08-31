d07ab96ed9d21628baf195c18444ded5f9fb90fb
LSY <ys_lee1017@naver.com>
2021-08-31 22:34:02 +0900

    2021 kakao blind recruitment

--- Programmers/kakao/2021_메뉴리뉴얼.py
+++ Programmers/kakao/2021_메뉴리뉴얼.py
@@ -1,31 +1,21 @@
 # https://programmers.co.kr/learn/courses/30/lessons/72411
-from collections import Counter, defaultdict
+from collections import defaultdict, Counter
 from itertools import combinations
 
+
 def solution(orders, course):
-    total = defaultdict(lambda : defaultdict(int))
-    for order in orders:
-        for count in course:
-            for comb in combinations(order, count):
-                total[count][comb] += 1
-    # print(total)
-    answer = []
-    print(total)
-    for key in total.keys():
-        tmp_cnt, tmp_list = 0, []
-        for menu, cnt in total[key].items():
-            if cnt > 1 and cnt > tmp_cnt:
-                tmp_list = ["".join(list(menu))]
-                tmp_cnt = cnt
-            elif cnt == tmp_cnt:
-                tmp_list.append("".join(list(menu)))
-        answer.extend(tmp_list)
-        # print(answer)
-    answer.sort()
-    return answer
+    ans = []
+    for count in course:
+        order_comb = defaultdict(int)
+        tmp = []
+        for order in orders:
+            tmp += combinations(sorted(order), count)
+        most_order = Counter(tmp).most_common()
+        # 최대 갯수 추출하는 방법
+        ans += [k for k, v in most_order if v > 1 and v == most_order[0][1]]
 
-
+    return sorted(["".join(val) for val in ans])
 
 
 # solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"], [2,3,4])
-solution(["XYZ", "XWY", "WXA"], [2,3,4])
+solution(["XYZ", "XWY", "WXA"], [2, 3, 4])
