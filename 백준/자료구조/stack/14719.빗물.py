d5f9313b6457779c1e7225c4487c9be41f5d96c5
LSY <ys_lee1017@naver.com>
2021-09-26 21:28:43 +0900

    스택

--- 백준/자료구조/stack/14719.빗물.py
+++ 백준/자료구조/stack/14719.빗물.py
@@ -6,18 +6,14 @@ h, w = map(int, read().split())
 blocks = list(map(int, read().split()))
 s = []
 ans = 0
-piv = -1
-for i, block in enumerate(blocks):
-    if piv == -1:
-        piv = block
-    elif piv > block:
-        s.append(block)
-    else:
+piv = blocks[0]
+for block in blocks[1:]:
+    if piv <= block:
         while s:
             tmp = s.pop()
-            if piv > tmp:
-                ans += piv - tmp
+            ans += piv - tmp
         piv = block
+    s.append(block)
 # 맨 마지막 벽을 만났을 경우
 piv = -1
 while s:
